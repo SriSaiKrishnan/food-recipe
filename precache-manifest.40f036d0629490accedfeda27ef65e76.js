77a65e7817f67e2b9244ddb07af012faed4ecb32
Saikrishnan <krishnansai99@gmail.com>
2020-08-04 00:27:58 +0530

    Updates

---
+++ precache-manifest.40f036d0629490accedfeda27ef65e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e47ad3ee3bd66de04413af586ace135",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "416f4bdda84dd57dba46",
+    "url": "/food-recipe/static/css/main.03cc9419.chunk.css"
+  },
+  {
+    "revision": "9dc3befe94308655a850",
+    "url": "/food-recipe/static/js/2.26689708.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.26689708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416f4bdda84dd57dba46",
+    "url": "/food-recipe/static/js/main.e29ad132.chunk.js"
+  },
+  {
+    "revision": "27ce8ef5f1ca62d23ae3",
+    "url": "/food-recipe/static/js/runtime-main.11e0e440.js"
+  }
+]);
